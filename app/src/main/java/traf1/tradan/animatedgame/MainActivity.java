10da72935115185c89d6d06e07ab20b3c2d69443
dltra <dltra@fcps.edu>
2019-09-22 19:41:28 -0400

    Initial commit

---
+++ app/src/main/java/traf1/tradan/animatedgame/MainActivity.java
@@ -0,0 +1,13 @@
+package traf1.tradan.animatedgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
